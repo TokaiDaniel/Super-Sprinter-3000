7c7d398116c44175e35fdb5df665994b28e0dbcf
Daniel Tokai <tokaidaniel@gmail.com>
2021-07-09 13:55:41 +0200

    Project initializations

---
+++ src/main/java/com/codecool/superSprinter/SuperSprinterApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.superSprinter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperSprinterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperSprinterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codecool/superSprinter/SuperSprinterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.superSprinter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperSprinterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
